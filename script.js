8ab873b98a76d8061cc0dd83fd112cf9abf31a80
JC2295 <jonathan.copeland@hotmail.co.uk>
2022-07-26 15:29:43 +0100

    Added arrays

---
+++ script.js
@@ -0,0 +1,11 @@
+function convertToRoman(num) {
+
+     var arabic_array = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+
+     var roman_array = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+
+
+    return num;
+   }
+   
+   console.log(convertToRoman(36)); 
