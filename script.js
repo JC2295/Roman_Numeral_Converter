504cb978f15bb70f2d4245f62aa7350b2ed6eeb7
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-06-05 11:32:38 +0000

    Added functionality

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 function convertToRoman(num) {
 
-     var arabic_array = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let arabicArray = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
-     var roman_array = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+  let romanArray = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+
+  let romanNumber = [];
+
+  for(let i = arabicArray.length - 1; i <= 0; i--){
+
+    if(num == arabicArray[i]){
+
+       romanNumber.push(romanArray[i]);
+
+      break;
+      
+
+    }
+    
+    
 
 
-    return num;
-   }
-   
-   console.log(convertToRoman(36)); 
+
+  
+
+    
+  
+    
+    
+
+    
+    
+    
+  }
+
+  
+  
+  return romanNumber.join("");
+}
+
+console.log(convertToRoman(40)); 
