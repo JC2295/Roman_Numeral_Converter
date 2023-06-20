3c331a675b35d02ffa115391d22c1250140fe77f
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-06-20 16:00:12 +0000

    Progress with roman numeral converter

---
+++ index.js
@@ -0,0 +1,40 @@
+function convertToRoman(num) {
+
+  let arabicArr = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+
+  let romanArr = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+
+  let arabicNum = num;
+
+  let romanNum = [];
+
+  for(let i = arabicArr.length; i >= 0; i--){
+
+    if(Math.floor(arabicNum / arabicArr[i]) > 0){
+
+      let divisorCount = Math.floor(arabicNum / arabicArr[i]);
+
+      console.log(divisorCount);
+
+      for(let j = 0; j < divisorCount; j++){
+        
+        romanNum.push(romanArr[i]);
+          
+      }
+
+      arabicNum -= (arabicNum % arabicArr[i]);
+      
+    }
+    else{
+      continue;
+    }
+  }
+
+  let romanNumStr = romanNum.join("");
+
+  console.log(romanNumStr);
+
+  return romanNumStr
+}
+
+convertToRoman(6);
